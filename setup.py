a5e21b46c6fe54a4f88a27917fc84f6278d87a32
chiddesh <chiddesh@gmail.com>
2025-08-31 17:25:34 +0530

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="filterKaapi",
-    version="0.1.1",
+    version="0.1.2",
     description="FilterKaapi: A Tamil-inspired programming language",
     long_description=long_description,
     long_description_content_type="text/markdown",
